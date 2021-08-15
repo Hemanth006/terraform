b67488053ae8a18fab049980f3445b7dfa7e9cd1
M.Hemanth <hemanth.mullagiri@gmail.com>
2021-08-15 20:12:06 +0530

    new to terraform

--- 03-example1/ec2.tf
+++ 03-example1/ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "sample" {
   ami                   = "ami-074df373d6bafa625"
-  instance_type         = "t3.micro"
+  instance_type         = "t2.micro"
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags                  = {
